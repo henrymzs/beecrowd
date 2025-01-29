8d53efb25bccb5a2323aaa8b348490aaf062379a
henrymzs <henrykaua21@gmai.com>
2025-01-29 14:22:17 -0300

    fix: add orientação 1044/1048 ao 1050

--- src/problemas/problema1044.js
+++ src/problemas/problema1044.js
@@ -1,3 +1,13 @@
+/*
+Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao Multiplos", indicando se os valores lidos são múltiplos entre si.
+
+Entrada
+A entrada contém valores inteiros.
+
+Saída
+A saída deve conter uma das mensagens conforme descrito acima.
+*/
+
 //let valores = lines.shift().split(' ');
 let A = 25; //parseInt(valores[0]);
 let B = 6; //parseInt(valores[1]);
